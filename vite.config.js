9e26f04c09a728f3c1d3e9362b8aa3f0abcad8dc
khushalGrover <khushal15grover@gmail.com>
2024-09-24 14:21:25 +0530

    trying to deploying

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'r3f-gameportfolio-ui',
+  base: 'R3F-gameportfolio-ui',
   plugins: [react()],
   server: {
     host: true
