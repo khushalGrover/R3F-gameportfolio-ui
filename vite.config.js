dd2f4ba0f10ae86d745f812189fba4ff37931913
khushalGrover <khushal15grover@gmail.com>
2024-10-12 11:40:53 +0530

    new config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'R3F-gameportfolio-ui',
+  base: process.env.GITHUB_PAGES ? '/R3F-gameportfolio-ui/' : '/',
   plugins: [react()],
   server: {
     host: true
